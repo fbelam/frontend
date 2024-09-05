73212171ef50b3632cd0c79cb4f34093654bfa4e
Fabricio Belam <36649120+fbelam@users.noreply.github.com>
2024-09-04 22:28:36 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Usercomponents from './userlist';
+
+function App() {
+  return (
+    <div className="App">
+      <Usercomponents />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/userlist.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import UserService from './serviceWorker';
+
+class UserComponent extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            users:[]
+        }
+    }
+
+    componentDidMount(){
+        UserService.getUsers().then((response) => {
+            this.setState({ users: response.data})
+        });
+    }
+
+    render (){
+        return (
+            <div>
+                <h1 className = "text-center"> Users List</h1>
+                <table className = "table table-striped">
+                    <thead>
+                        <tr>
+
+                            <td> Id</td>
+                            <td> First Name</td>
+                            <td> Last Name</td>
+                            <td> Email</td>
+                        </tr>
+
+                    </thead>
+                    <tbody>
+                        {
+                            this.state.users.map(
+                                user => 
+                                <tr key = {user.id}>
+                                     <td> {user.id}</td>   
+                                     <td> {user.firstName}</td>   
+                                     <td> {user.lastName}</td>   
+                                     <td> {user.email}</td>   
+                                </tr>
+                            )
+                        }
+
+                    </tbody>
+                </table>
+
+            </div>
+
+        )
+    }
+}
+
+export default UserComponent
